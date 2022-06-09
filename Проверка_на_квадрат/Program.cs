7d35965af7a78ddc276e53ba84ba70a2009faae5
Zrelova <zrelovak1994@mail.ru>
2022-06-09 17:12:05 +0300

    Семинар1

---
+++ Проверка_на_квадрат/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите первое число: ");
+string input1 = Console.ReadLine ();
+int number1 = Convert.ToInt32(input1);
+
+Console.WriteLine ("Введите второе число: ");
+string input2 = Console.ReadLine ();
+int number2 = Convert.ToInt32(input2);
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine (number2 + "*" + number2 + "=" + number1 + " TRUE");
+}
+else
+{
+    Console.WriteLine (number2 + "*" + number2 + "=" + number1 + " FALSE");
+}
