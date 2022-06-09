9083917edd2e660f9ef9692d4c060d878ca4a565
Zrelova <zrelovak1994@mail.ru>
2022-06-09 17:33:39 +0300

    seminar1

---
+++ Квадрат_числа/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write ("Введите число: ");
+string input = Console.ReadLine ();
+int number  = Convert.ToInt32(input);
+
+int result = number * number;
+
+Console.WriteLine (number + " в квадрате " + result);
---
+++ Последняя_цифра/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите число: ");
+string input = Console.ReadLine ();
+int number = Convert.ToInt32(input);
+if (number > 99 && number < 1000)
+{
+    int lastNumber = number % 10;
+    Console.WriteLine("Последняя цифра вашего числа: " + lastNumber);
+}
+else
+{
+   Console.WriteLine("Число неверно"); 
+}
