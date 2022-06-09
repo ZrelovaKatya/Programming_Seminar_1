1057db2537a746ce75849b811fa7725f45cc8231
Zrelova <zrelovak1994@mail.ru>
2022-06-09 17:17:58 +0300

    Семинар1

---
+++ Промежуток_от_-ч_+ч/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите положительное число: ");
+string input = Console.ReadLine ();
+int number = Convert.ToInt32(input);
+int min = -number;
+
+while (min <= number)
+{
+    Console.Write(min + " ");
+    min ++;
+}
